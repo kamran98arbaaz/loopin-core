c368524229601a8715cd04ae7de4275c885ed3e8
arbaz5kamran <arbaz5kamran@gmail.com>
2025-08-31 23:28:56 +0530

    Bug fixes

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
 from app import app
 
-# Vercel expects the Flask app to be named 'app'
+# Export the app for Vercel Python runtime
+application = app
+
+# Vercel expects the Flask app to be named 'app' or 'application'
 # This file serves as the entry point for Vercel serverless functions
